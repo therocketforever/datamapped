365f5cb36664446be636609a08b4e61c2003f1fa
DON who is a ghost <therocketforever@me.com>
2013-09-30 17:17:30 -0400

    Make some users to play with

--- app.rb
+++ app.rb
@@ -1,29 +1,50 @@
-DataMapper.setup :default, 'sqlite::memory:'
-
-class User
-  include DataMapper::Resource
-  property :id, Serial
-  property :name, String
-  property :number, Integer, reader: :protected
-end
-
-DataMapper.finalize.auto_migrate!
+#DataMapper.setup(:default, "sqlite://#{Dir.pwd}/development.db")
+#DataMapper.setup :default, 'sqlite::memory:'
 
 class Web < Sinatra::Base
 
+  #configure(:development) { DataMapper.setup :default, 'sqlite::memory:' }
 
   get '/' do
-    puts "Hello world."
-    binding.pry
+    @user = User.first unless User.all.empty? unless @user
+    if @user then "Hello #{@user.name}! Your number is #{@user.number}." else redirect '/init' end
   end
+
+  get '/init' do
+    User.first_or_create( :name =>"Jim Kirk", :number =>"123" )
+    User.first_or_create( :name =>"Lenord McCoy", :number =>"456" )
+    User.first_or_create( :name =>"Spock", :number =>"789" )
+    redirect '/'
+  end
+
+  get '/inspect' do
+    binding.pry
+    redirect '/'
+  end
+
 end
 
 class API < Grape::API
+
+  #DataMapper.setup :default, 'sqlite::memory:'
+  
   namespace :users do
+    
     get '/:id' do
       User.get params[:id]
     end
+  
   end
+
 end
 
-binding.pry
+class User
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :name, String
+  property :number, Integer #, reader: :protected
+
+end
+
+DataMapper.finalize.auto_migrate!
--- config.ru
+++ config.ru
@@ -6,6 +6,8 @@ configure(:production)  { Bundler.require :production  }
 configure(:development) { Bundler.require :development }
 configure(:test)        { Bundler.require :test }
 
+DataMapper.setup :default, 'sqlite::memory:'
+
 require File.expand_path('../app', __FILE__)
 
 run Rack::Cascade.new [API, Web]
